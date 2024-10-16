5418fba989673697b0bf864d60229ad55b9599dc
pkiop <pkiopb@gmail.com>
2024-10-16 23:30:21 +0900

    reverse_string

---
+++ src/string.rs
@@ -0,0 +1,13 @@
+pub fn reverse_string(s: &str) -> String {
+    s.chars().rev().collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_reverse_string() {
+        assert_eq!(reverse_string("hello"), "olleh");
+    }
+}
