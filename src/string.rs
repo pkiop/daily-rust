ce64793663df70bcecf170d6002b0c1c8884dd44
pkiop <pkiopb@gmail.com>
2024-10-17 23:48:08 +0900

    add split_string

--- src/string.rs
+++ src/string.rs
@@ -2,6 +2,10 @@ pub fn reverse_string(s: &str) -> String {
     s.chars().rev().collect()
 }
 
+pub fn split_string(s: &str, delimiter: char) -> Vec<String> {
+    s.split(delimiter).map(|x|  x.to_string()).collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -9,5 +13,10 @@ mod tests {
     #[test]
     fn test_reverse_string() {
         assert_eq!(reverse_string("hello"), "olleh");
+    }
+
+    #[test]
+    fn test_split_string() {
+        assert_eq!(split_string("a,b", ','), ["a", "b"])
     }
 }
